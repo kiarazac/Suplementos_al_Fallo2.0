95e8d0e94375bf5b87e0a6b54fc1cadf9f2ed7a6
Kiara Zacarias <kiarazac05@gmail.com>
2026-06-03 20:29:36 -0300

    Guardo mis cambios antes de actualizar

--- resources/views/catalogo/index.blade.php
+++ resources/views/catalogo/index.blade.php
@@ -257,8 +257,5 @@
 
 
 @section('scripts')
-
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}">
-</script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    
 
     @yield('page-css')
 </head>
@@ -28,7 +29,6 @@
     @include('partials.footer')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
     <!-- Scripts -->
     @yield('scripts')
 
